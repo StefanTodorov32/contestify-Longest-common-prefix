c0405ae221f864bc98c8dbb8f94e397e497d5918
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2025-05-08 14:00:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,22 @@
  * @return {string}
  */
 function longestCommonPrefix(strs) {
+    if (strs.length === 0) return "";
     
+    // Find the shortest string in the array
+    let shortest = strs.reduce((a, b) => a.length <= b.length ? a : b);
+    
+    // Check each character of the shortest string
+    for (let i = 0; i < shortest.length; i++) {
+        // Check if this character is common to all strings
+        for (let str of strs) {
+            if (str[i] !== shortest[i]) {
+                return shortest.substring(0, i);
+            }
+        }
+    }
+    
+    return shortest;
 }
 
 module.exports = longestCommonPrefix;
